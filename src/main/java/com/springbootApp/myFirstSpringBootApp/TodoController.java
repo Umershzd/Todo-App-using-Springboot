221bbf6f48aa9dec5435e36fcf6d44d9ec4d602b
Umershzd <m.umer.shehzad@hotmail.com>
2024-06-11 17:12:50 +0500

    adding spring security into welcome controller and todo controller

--- src/main/java/com/springbootApp/myFirstSpringBootApp/TodoController.java
+++ src/main/java/com/springbootApp/myFirstSpringBootApp/TodoController.java
@@ -2,6 +2,8 @@ package com.springbootApp.myFirstSpringBootApp;
 import java.time.LocalDate;
 
 import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -23,14 +25,20 @@ public class TodoController {
     @RequestMapping("Todos-List")
 public String listAllTodos(ModelMap model)
 {
-List<Todo> todos =todoService.findByUsername("Umer");
+    String username = getName(model);
+    List<Todo> todos =todoService.findByUsername(username);
 model.addAttribute("todos",todos);
     return "ListTodos";
 }
+
+    private static String getName(ModelMap model) {
+        return (String) model.get("name");
+    }
+
     @RequestMapping(value="add-todo",method = RequestMethod.GET)
     public String showNewTodoPage(ModelMap model)
     {
-        String username = (String) model.get("name");
+        String username = getLoggedinUsername(model);
 Todo todo=new Todo(0,username,"default desc",LocalDate.now().plusYears(1),false);
 model.put("todo",todo);
 return "Todo";
@@ -42,7 +50,7 @@ return "Todo";
         {
             return "Todo";
         }
-            String username = (String) model.get("name");
+            String username = getLoggedinUsername(model);
             todoService.addTodo(username, todo.getDescription(), todo.getTargetdate(), false);
             return "redirect:Todos-List";
 
@@ -68,10 +76,15 @@ model.addAttribute("todo",todo);
         {
             return "Todo";
         }
-        String username = (String) model.get("name");
+        String username = getLoggedinUsername(model);
         todo.setUsername(username);
         todoService.updateTodo(todo);
         return "redirect:Todos-List";
 
+    }
+    private String getLoggedinUsername(ModelMap model)
+    {
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 }
--- src/main/java/com/springbootApp/myFirstSpringBootApp/WelcomeController.java
+++ src/main/java/com/springbootApp/myFirstSpringBootApp/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.springbootApp.myFirstSpringBootApp;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,31 +11,19 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @Controller
 @SessionAttributes("name")
 public class WelcomeController {
-//    private  AuthenticateService authenticateService;
-//
-//    public loginController(AuthenticateService authenticateService) {
-//        this.authenticateService = authenticateService;
-//    }
 
     @RequestMapping(value="/",method = RequestMethod.GET)
     public String gotoWelcomePage(ModelMap model)
     {
         System.out.println("Hello login page Get method");
-        model.put("name", "MUS");
+        model.put("name", getLoggedinUsername());
         return "welcome";
-    }
-//    @RequestMapping(value="login",method = RequestMethod.POST)
-//    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model)
-//    {
-//        if (authenticateService.authenticate(name,password)) {
-//            model.put("name", name);
-//
-//            System.out.println("Hello login page POST");
-//            return "welcome";
-//        }
-//        model.put("errormessage","Invalid Credentials , please try again...!");
-//        return "login";
-//    }
 
+    }
+    private String getLoggedinUsername() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
 
 }
