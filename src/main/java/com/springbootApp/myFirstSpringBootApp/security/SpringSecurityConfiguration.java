e00bd713510465388356bfc008ed8e8713552bd3
Umershzd <m.umer.shehzad@hotmail.com>
2024-07-06 17:36:23 +0500

    Spring Security
    Disabling the CSFR, frames to connect the h2-database with the application using provided interface of SecurityFilterChain and Custumizable defaults

--- src/main/java/com/springbootApp/myFirstSpringBootApp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/springbootApp/myFirstSpringBootApp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.springbootApp.myFirstSpringBootApp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 import java.util.function.Function;
 
 @Configuration
@@ -40,4 +43,14 @@ public class SpringSecurityConfiguration {
          return new BCryptPasswordEncoder();
         }
 
+@Bean
+    public SecurityFilterChain filterchain(HttpSecurity http) throws Exception
+{
+http.authorizeHttpRequests(
+        auth->auth.anyRequest().authenticated());
+http.formLogin(withDefaults());
+http.csrf().disable();
+http.headers().frameOptions().disable();
+return http.build();
+}
 }
