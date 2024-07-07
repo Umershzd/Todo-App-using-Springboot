1ccde327ccab6cd934a639763efec322b5f9d33f
Umershzd <m.umer.shehzad@hotmail.com>
2024-07-07 12:13:52 +0500

    Configuring the h2-database and adding the data into h2-database
    The data in h2-database always destroyed thats why we set the properties to spring.jpa.defer-datasource-initialization=true also we populate the data by creating the data.sql file and make sure that the file must be present in main/src/resources

--- src/main/java/com/springbootApp/myFirstSpringBootApp/Todo.java
+++ src/main/java/com/springbootApp/myFirstSpringBootApp/Todo.java
@@ -1,11 +1,18 @@
 package com.springbootApp.myFirstSpringBootApp;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
-
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue
     private int id;
+
     private String username;
 @Size(min=10,message = "Please enter the description...!")
     private String description;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+values(10001,'Coding Roadmap', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+values(10002,'Coding Roadmap', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+values(10003,'Coding Roadmap', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+values(10004,'Coding Roadmap', 'Learn DevOps', CURRENT_DATE(), false);
