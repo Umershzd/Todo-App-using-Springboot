404befa192a171d726635c06a7557d58b2d1a163
Umershzd <m.umer.shehzad@hotmail.com>
2024-07-07 20:23:25 +0500

    Configuring the Docker and Updating the data in mysql data base
    Updating the application properties, setting up the docker container for application and changing the insert command in data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+insert ignore into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
 values(10001,'Umer', 'Get AWS Certified', CURRENT_DATE(), false);
 
-insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+insert ignore into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
 values(10002,'Umer', 'Get Azure Certified', CURRENT_DATE(), false);
 
-insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+insert ignore into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
 values(10003,'Umer', 'Get GCP Certified', CURRENT_DATE(), false);
 
-insert into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
+insert ignore into todo (ID, USERNAME, DESCRIPTION, TARGETDATE, DONE)
 values(10004,'Umer', 'Learn DevOps', CURRENT_DATE(), false);
